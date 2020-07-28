43161e9cca0290035ab171921f9df8361e6e7d83
Kaio Henrique Fernandes <kaiohfernandes15@gmail.com>
2020-07-28 00:40:03 -0300

    Ajustando email

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = withImages({
   env: {
     MJ_APIKEY_PUBLIC: '8f56daf2dc17cfeef21f8c7456783671',
     MJ_APIKEY_PRIVATE: 'fd1ef9cf68813cf3e52f96b35c8b36dc',
-    EMAIL_SENDER: 'kaiohfernandes15@gmail.com',
-    EMAIL_RECEIVER: 'jefferson@consultx.net.br'
+    EMAIL_SENDER: 'sistema@consultx.net.br',
+    EMAIL_RECEIVER: 'kaiohfernandes15@gmail.com'
   }
 })
