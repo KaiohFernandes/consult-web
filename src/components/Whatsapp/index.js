a940131fac7776ab457906da4ced5f0134ab32e5
Kaio Henrique Fernandes <kaiohfernandes15@gmail.com>
2020-09-21 16:20:50 -0300

    Change cellphone

--- src/components/Whatsapp/index.js
+++ src/components/Whatsapp/index.js
@@ -3,7 +3,7 @@ import whatsapp from '../../assets/images/whatsapp-white.svg';
 
 function Whatsapp() {
   return (
-    <Link href="https://api.whatsapp.com/send?phone=5547991424263&text=Olá Consult" target="_blank">
+    <Link href="https://api.whatsapp.com/send?phone=5547997882138&text=Olá Consult" target="_blank">
       <img src={whatsapp} alt="Whatsapp" title="Whatsapp" />
     </Link>
   )
